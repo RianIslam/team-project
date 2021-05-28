3e600aa5f4b33719c97689fabd1fb49c0f1c995b
RianIslam <rianislamdr@gmail.com>
2021-05-29 02:40:13 +0600

    slide bar doing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 const Header = () => {
     return (
         <Container>
@@ -19,6 +20,20 @@ const Header = () => {
                 <a href="#">Lambor Account</a>
             </RightMenu>
 
+            <BurgerNav>
+            <CloseWrapper>
+            <CustomClose/>
+            </CloseWrapper>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Inventory</a></li>
+                <li><a href="#">Trade-in</a></li>
+                <li><a href="#">Cybertruck</a></li>
+                <li><a href="#">Roadaster</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+            </BurgerNav>
+
             <CustomMenu/>
         </Container>
     )
@@ -31,10 +46,12 @@ min-height: 60px;
 position: fixed;
 display: flex;
 align-items: center;
+justify-content: space-between;
 padding: 0 20px;
 top: 0;
 left: 0;
 right: 0;
+z-index:10;
 
 `
 
@@ -51,10 +68,15 @@ a{
     flex-wrap: nowrap;
 }
 
+@media(max-width: 768px){
+    display: none;
+}
+
 `
 
 const RightMenu = styled.div`
-
+display: flex;
+align-items: center;
 a{
     font-weight:600;
     text-transform: uppercase;
@@ -65,5 +87,40 @@ a{
 `
 
 const CustomMenu = styled(MenuIcon)`
+cursor: pointer;
+`
 
+const BurgerNav = styled.div`
+position: fixed;
+top: 0;
+bottom: 0;
+right: 0;
+background: purple;
+width: 300px;
+z-index:16;
+list-style: none;
+padding: 20px;
+display: flex;
+flex-direction: column;
+text-align:start;
+li{
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0,0,.2);
+    a{
+        font-weight:600;
+    }
+}
+ 
+`
+
+const CustomClose = styled(CloseIcon)`
+
+cursor: pointer;
+`
+
+const CloseWrapper =styled.div`
+
+
+display: flex;
+justify-content: flex-end;
 `
